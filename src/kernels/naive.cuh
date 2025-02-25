51f93e51c0d89924a231b508ec68e570c4375c6b
Suhith Rajesh <10973031+suhithr@users.noreply.github.com>
2025-02-24 20:01:51 -0500

    Initial commit

---
+++ src/kernels/naive.cuh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdio>
+#include <cuda_runtime.h>
+
+/*
+Naive:
+MxK * KxN = MxN
+*/
+__global__ void sgemm_naive(int M, int N, int K, float alpha, const float *A,
+    const float *B, float beta, float *C) {
+        const uint x = blockIdx.x * blockDim.x + threadIdx.x;
+        const uint y = blockIdx.y * blockDim.y + threadIdx.y;
+    
+        if (x < M && y < N) {
+            float tmp = 0.0;
+            for (int i = 0; i < K; ++i) {
+                tmp += A[x * K + i] + B[i * N + y];
+            }
+            C[x * N + y] = alpha * tmp + beta * C[x * N + y];
+        }
+    }
