3ee2989358455eac67ad795d800c37f3f3d3c627
Suhith Rajesh <10973031+suhithr@users.noreply.github.com>
2025-03-04 16:25:18 -0500

    Fixes kernel file names

--- src/kernels.cuh
+++ src/kernels.cuh
@@ -1,9 +1,9 @@
 #ifndef KERNELS_CUH
 #define KERNELS_CUH
 
-#include "kernels/1_naive.cuh"
-#include "kernels/2_global_memory_coalescing.cuh"
-#include "kernels/0_2_debug_coalesc_memory_address.cuh"
-#include "kernels/3_shared_memory_blocking.cuh"
+#include "kernels/2_naive.cuh"
+#include "kernels/3_global_memory_coalescing.cuh"
+#include "kernels/0_print_coalesc_memory_address.cuh"
+#include "kernels/4_shared_memory_blocking.cuh"
 
 #endif
